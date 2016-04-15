25446a8289c39e078ef1ada3bad31aa792c4516e
trackstar200 <tim.stoddard2@gmail.com>
2016-04-15 01:20:53 -0700

    refactored and added strict mode to home.js

--- js/home.js
+++ js/home.js
@@ -1,70 +1,13 @@
+'use strict';
+
 $(function() {
-    
+
     $('.shadow-header').html('TIM STODDARD');
     if ($().slideToggle) {
         $('.detail-text').hide();
         $('td > span').addClass('glyphicon-chevron-down');
     }
-    console.log($().slideToggle);
     initPositionShadow();
-    
-    $('#contact').click(function() {
-        let phone = ['1','2','4','1',' ','4','9','3',' ','8','0','4'];
-        let school = ['u','d','e','.','y','l','o','p','l','a','c','@','r','a','d','d','o','t','s','t'];
-        let work = ['t','i','.','e','t','a','e','r','C','o','S','@','d','r','a','d','d','o','t','S','.','y','h','t','o','m','i','T'];
-        let personal = ['m','o','c','.','l','i','a','m','g','@','2','d','r','a','d','d','o','t','s','.','m','i','t'];
-        $('#phone').html(`${phone.reverse().join('')}`)
-        $('#school').html(`<a href="mailto:${school.reverse().join('')}">${school.join('')}</a>`);
-        $('#work').html(`<a href="mailto:${work.reverse().join('')}">${work.join('')}</a>`);
-        $('#personal').html(`<a href="mailto:${personal.reverse().join('')}">${personal.join('')}</a>`);
-        $('#myModal').modal();
-    });
-    
-    $('#send').click(function() {
-        if (!$('#name').val()) {
-            alert('Please enter your name.');
-            return;
-        }
-        if (!$('#sender-email').val()) {
-            alert('Please enter your email.');
-            return;
-        }
-        if (!$('#subject').val()) {
-            alert('Please enter a subject.');
-            return;
-        }
-        if (!$('#message').val()) {
-            alert('Please enter a message.');
-            return;
-        }
-        //console.log(`"${$('#name').val()}" (${$('#sender-email').val()})`);
-        $.ajax({
-            type: 'POST',
-            url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-            data: {
-                'key': 'J5O1Hvb_BAVJa73BtCJm3g',
-                'message': {
-                    'from_email': 'homepage-contact@tstoddard.com',
-                    'from_name': `${$('#name').val()}`,
-                    'to': [
-                        {
-                            'email': 'tim.stoddard2@gmail.com',
-                            'name': 'Tim',
-                            'type': 'to'
-                        }
-                    ],
-                    'subject': `${$('#subject').val()}`,
-                    'html': `"${$('#name').val()}" (${$('#sender-email').val()}) says: ${sanitizeText($('#message').val())}`
-                }
-            }
-        }).done(function() {
-            $('#myModal').modal('hide');
-            $('#name').val('');
-            $('#sender-email').val('');
-            $('#subject').val('');
-            $('#message').val('');
-        });
-    });
 
 });
 
@@ -90,19 +33,10 @@ function positionShadow() {
     var y = header.position().top;
     var wid = header.width();
     var hgt = header.height();
-    var shadowHeader = $('.shadow-header');
-    var sh_x = shadowHeader.position().left;
-    var sh_y = shadowHeader.position().top;
-    var sh_wid = shadowHeader.width();
-    var sh_hgt = shadowHeader.height();
     $('.shadow-header').css({
         top: y + hgt * 0.2,
         left: x + wid * 0.14
     });
-}
-
-function sanitizeText(str) {
-    return document.createElement('div').appendChild(document.createTextNode(str)).innerHTML;
 }
 
 $('td > span').click(function() {
@@ -121,14 +55,15 @@ $('td > span').click(function() {
                     positionShadow();
                 },
                 done: function() {
-                    //$('td > span').addClass('hover-wiggle');
+                    // $('td > span').addClass('hover-wiggle');
                     $('td > span').addClass('semi-transparent');
                 }
             });
     } else {
         $('td > span').removeClass('hover-wiggle');
         $('td > span').removeClass('semi-transparent');
-        $('.detail-text').slideToggle({
+        $('.detail-text').slideToggle(
+            {
                 duration: 500,
                 progress: function(anim, prog) {
                     $('td > span').css({
@@ -143,3 +78,69 @@ $('td > span').click(function() {
             });
     }
 });
+
+$('#contact').click(function() {
+    if ($().modal) {
+        var phone = ['1','2','4','1',' ','4','9','3',' ','8','0','4'];
+        var school = ['u','d','e','.','y','l','o','p','l','a','c','@','r','a','d','d','o','t','s','t'];
+        var work = ['t','i','.','e','t','a','e','r','C','o','S','@','d','r','a','d','d','o','t','S','.','y','h','t','o','m','i','T'];
+        var personal = ['m','o','c','.','l','i','a','m','g','@','2','d','r','a','d','d','o','t','s','.','m','i','t'];
+        $('#phone').html(`${phone.reverse().join('')}`)
+        $('#school').html(`<a href="mailto:${school.reverse().join('')}">${school.join('')}</a>`);
+        $('#work').html(`<a href="mailto:${work.reverse().join('')}">${work.join('')}</a>`);
+        $('#personal').html(`<a href="mailto:${personal.reverse().join('')}">${personal.join('')}</a>`);
+        $('#myModal').modal();
+    } else {
+        window.open('contact.html');
+    }
+});
+
+$('#send').click(function() {
+    if (!$('#name').val()) {
+        alert('Please enter your name.');
+        return;
+    }
+    if (!$('#sender-email').val()) {
+        alert('Please enter your email.');
+        return;
+    }
+    if (!$('#subject').val()) {
+        alert('Please enter a subject.');
+        return;
+    }
+    if (!$('#message').val()) {
+        alert('Please enter a message.');
+        return;
+    }
+    //console.log(`"${$('#name').val()}" (${$('#sender-email').val()})`);
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+            'key': 'J5O1Hvb_BAVJa73BtCJm3g',
+            'message': {
+                'from_email': 'homepage-contact@tstoddard.com',
+                'from_name': `${$('#name').val()}`,
+                'to': [
+                    {
+                        'email': 'tim.stoddard2@gmail.com',
+                        'name': 'Tim',
+                        'type': 'to'
+                    }
+                ],
+                'subject': `${$('#subject').val()}`,
+                'html': `"${$('#name').val()}" (${$('#sender-email').val()}) says: ${sanitizeText($('#message').val())}`
+            }
+        }
+    }).done(function() {
+        $('#myModal').modal('hide');
+        $('#name').val('');
+        $('#sender-email').val('');
+        $('#subject').val('');
+        $('#message').val('');
+    });
+});
+
+function sanitizeText(str) {
+    return document.createElement('div').appendChild(document.createTextNode(str)).innerHTML;
+}
