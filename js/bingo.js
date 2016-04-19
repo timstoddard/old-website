35db640aa59b66cc9f228d451698e1284e224606
trackstar200 <tim.stoddard2@gmail.com>
2016-04-19 14:02:56 -0700

    added table grid

--- js/bingo.js
+++ js/bingo.js
@@ -6,12 +6,30 @@ $(function() {
 	})
 	.done(function(response) {
 		var arr = response.data.children;
-		console.log(arr);
+		// console.log(arr);
 		var htmlStr = '';
+		var table = [[], [], [], [], []];
+		var row = 0;
+		var col = 0;
 		arr.forEach(function(elem) {
 			htmlStr += `<li><img src="${elem.data.thumbnail}"> ${elem.data.title} (<a href="${elem.data.url}">link</a>)</li>`;
+			table[row][col] = elem.data.thumbnail;
+			col++;
+			if (col == 5) {
+				row++;
+				col = 0;
+			}
 		});
-		$('.list-container').html(htmlStr);
+		$('.list-container').html(`<ul>${htmlStr}</ul>`);
+		var tableStr = '';
+		for (var i = 0; i < table.length; i++) {
+			tableStr += '<tr>';
+			for (var j = 0; j < table.length; j++) {
+				tableStr += `<td><img src="${table[i][j]}"></td>`
+			}
+			tableStr += '</tr>';
+		}
+		$('.bingo-table').html(`<table>${tableStr}</table>`);
 	})
 	.fail(function(xhr, status, errorThrown) {
 		alert( "Sorry, there was a problem!" );
